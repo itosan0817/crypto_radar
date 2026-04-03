e0c0d3690a20f0114a03d8bb5e35c296a71e696d
itosan0817 <yoshifumi.ito2@gmail.com>
2026-04-03 18:42:36 +0900

    0403

---
+++ install_services.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+sudo mv /home/yoshi/*.service /etc/systemd/system/
+sudo systemctl daemon-reload
+sudo pkill -f aerodrome_radar.py
+sudo pkill -f bribe_sniper.py
+sleep 2
+
+sudo systemctl enable aerodrome-radar.service
+sudo systemctl enable bribe-sniper.service
+
+sudo systemctl restart aerodrome-radar.service
+sudo systemctl restart bribe-sniper.service
+
+sudo systemctl status aerodrome-radar.service --no-pager
+sudo systemctl status bribe-sniper.service --no-pager
